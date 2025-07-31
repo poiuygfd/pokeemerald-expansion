9430c8c8893fc59d4ea111451aaefbe8c81b49ad
poiuygfd <jamphillips2000@gmail.com>
2025-07-31 01:55:12 +0100

    tent changes
    changed the pool of mons in the verdanturf and fallarbor tents to use the new dex and utilise the mons planned, currently they just reuse sets from the slateport tent but might give them unique sets eventually.

--- include/constants/vars.h
+++ include/constants/vars.h
@@ -95,7 +95,7 @@
 #define VAR_POKELOT_RND1                                 0x404B
 #define VAR_POKELOT_RND2                                 0x404C
 #define VAR_POKELOT_PRIZE_PLACE                          0x404D
-#define VAR_UNUSED_0x404E                                0x404E // Unused Var
+#define VAR_GENESECT_DRIVE_STATE                         0x404E // For Genesect Drive gifts, so the player doesn't get repeat drives if their Bag is full
 #define VAR_LOTAD_SIZE_RECORD                            0x404F
 #define VAR_LITTLEROOT_TOWN_STATE                        0x4050
 #define VAR_OLDALE_TOWN_STATE                            0x4051
