dfabf84cb5e324dd31ddbc3d4ebc5ea717730f24
poiuygfd <jamphillips2000@gmail.com>
2025-12-05 00:05:09 +0000

    yup, even more split path stuff
    the grind continues

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -694,8 +694,8 @@
 #define FLAG_HIDE_ROUTE_106_SPELUNKER  0x285 // Scaredy spelunker outside Granite Cave
 #define FLAG_HIDE_DEWFORD_TOWN_SPELUNKER  0x286 // Scaredy spelunker fled to Dewford Town
 #define FLAG_HIDE_ROUTE_111_BATTLERS  0x287 // Battler obstacles on Route 111
-#define FLAG_UNUSED_0x288  0x288 // Unused Flag
-#define FLAG_UNUSED_0x289  0x289 // Unused Flag
+#define FLAG_HIDE_SLATEPORT_CITY_MAGMA_GRUNTS  0x288 // Magma Grunt Blockade in Slateport
+#define FLAG_HIDE_ABRA_TELEPORT_MAN  0x289 // Man who will let the player warp between Mauville and Dewford
 #define FLAG_UNUSED_0x28A  0x28A // Unused Flag
 #define FLAG_UNUSED_0x28B  0x28B // Unused Flag
 #define FLAG_UNUSED_0x28C  0x28C // Unused Flag
